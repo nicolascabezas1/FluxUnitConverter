04628ad486d73f5429264bb3b772f229776dcb86
Nicolás Cabezas <cabezasn628@gmail.com>
2023-10-11 19:25:49 -0500

    :seedling: add see files creating the main values such as colors and text strings to use as the project progresses more will be added

--- app/src/main/java/com/android/fluxunitconverter/MainActivity.kt
+++ app/src/main/java/com/android/fluxunitconverter/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.fluxunitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.fluxunitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // ViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
